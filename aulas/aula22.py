44457dd15953383974a75674604b160f69aed8aa
Edu-Argolo-Guerreiro <105218997+Edu-Argolo-Guerreiro@users.noreply.github.com>
2023-11-20 18:07:22 +0000

    +1

---
+++ aulas/aula22.py
@@ -0,0 +1,12 @@
+#aula 22 ----------------------------------------
+
+#dicionario
+aluno = {
+    'nome': 'Edu',
+    'idade': 23,
+    'curso': 'Sistemas de Informação',
+    'Aprovação': True
+}
+
+for keys, values in aluno.items():
+    print(keys, values)
